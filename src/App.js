3876945868116e5a5dc3eb782f034bf667884d6a
MaryDreichan <maria@texo.by>
2023-07-16 15:27:16 +0300

    add Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./assets/components/Home";
+import Game from "./assets/components/Game";
+import Table from "./assets/components/Table"
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Home />
+      <Game />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
